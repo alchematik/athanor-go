b5af9e1ef109e65b3dae12576ef31c3deed00d4f
Kohei Hisakuni <khisakuni@gmail.com>
2024-01-11 10:09:12 -0800

    Move plugin logic to sdk

--- example/provider/main.go
+++ example/provider/main.go
@@ -4,13 +4,8 @@ import (
 	"context"
 
 	provider "github.com/alchematik/athanor-go/example/schema/output"
-	providerpb "github.com/alchematik/athanor-go/internal/gen/go/proto/provider/v1"
+	plugin "github.com/alchematik/athanor-go/sdk/provider/plugin"
 	sdk "github.com/alchematik/athanor-go/sdk/provider/value"
-
-	"github.com/hashicorp/go-plugin"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 type Server struct {
@@ -115,137 +110,19 @@ func (b BucketObject) DeleteBucketObject(ctx context.Context, id provider.Bucket
 	return nil
 }
 
-func (s *Server) GetResource(ctx context.Context, req *providerpb.GetResourceRequest) (*providerpb.GetResourceResponse, error) {
-	t := req.GetIdentifier().GetIdentifier().GetType()
-	handler, ok := s.ResourceHandlers[t]
-	if !ok {
-		return &providerpb.GetResourceResponse{}, status.Error(codes.NotFound, "resource type not found")
-	}
-
-	id, err := sdk.ParseIdentifierProto(req.GetIdentifier().GetIdentifier())
-	if err != nil {
-		return &providerpb.GetResourceResponse{}, status.Error(codes.Internal, err.Error())
-	}
-
-	res, err := handler.GetResource(ctx, id)
-	if err != nil {
-		return &providerpb.GetResourceResponse{}, status.Error(codes.Internal, err.Error())
-	}
-
-	return &providerpb.GetResourceResponse{Resource: res.ToResourceProto()}, nil
-}
-
-func (s *Server) CreateResource(ctx context.Context, req *providerpb.CreateResourceRequest) (*providerpb.CreateResourceResponse, error) {
-	t := req.GetIdentifier().GetIdentifier().GetType()
-	handler, ok := s.ResourceHandlers[t]
-	if !ok {
-		return &providerpb.CreateResourceResponse{}, status.Error(codes.NotFound, "resource type not found")
-	}
-
-	id, err := sdk.ParseIdentifierProto(req.GetIdentifier().GetIdentifier())
-	if err != nil {
-		return &providerpb.CreateResourceResponse{}, status.Error(codes.Internal, err.Error())
-	}
-
-	config, err := sdk.ParseProto(req.GetConfig())
-	if err != nil {
-		return &providerpb.CreateResourceResponse{}, status.Error(codes.Internal, err.Error())
-	}
-
-	res, err := handler.CreateResource(ctx, id, config)
-	if err != nil {
-		return &providerpb.CreateResourceResponse{}, status.Error(codes.Internal, err.Error())
-	}
-
-	return &providerpb.CreateResourceResponse{Resource: res.ToResourceProto()}, nil
-}
-
-func (s *Server) UpdateResource(ctx context.Context, req *providerpb.UpdateResourceRequest) (*providerpb.UpdateResourceResponse, error) {
-	t := req.GetIdentifier().GetIdentifier().GetType()
-	handler, ok := s.ResourceHandlers[t]
-	if !ok {
-		return &providerpb.UpdateResourceResponse{}, status.Error(codes.NotFound, "resource type not found")
-	}
-
-	id, err := sdk.ParseIdentifierProto(req.GetIdentifier().GetIdentifier())
-	if err != nil {
-		return &providerpb.UpdateResourceResponse{}, status.Error(codes.Internal, err.Error())
-	}
-
-	config, err := sdk.ParseProto(req.GetConfig())
-	if err != nil {
-		return &providerpb.UpdateResourceResponse{}, status.Error(codes.Internal, err.Error())
-	}
-
-	res, err := handler.UpdateResource(ctx, id, config)
-	if err != nil {
-		return &providerpb.UpdateResourceResponse{}, status.Error(codes.Internal, err.Error())
-	}
-
-	return &providerpb.UpdateResourceResponse{Resource: res.ToResourceProto()}, nil
-}
-
-func (s *Server) DeleteResource(ctx context.Context, req *providerpb.DeleteResourceRequest) (*providerpb.DeleteResourceResponse, error) {
-	t := req.GetIdentifier().GetIdentifier().GetType()
-	handler, ok := s.ResourceHandlers[t]
-	if !ok {
-		return &providerpb.DeleteResourceResponse{}, status.Error(codes.NotFound, "resource type not found")
-	}
-
-	id, err := sdk.ParseIdentifierProto(req.GetIdentifier().GetIdentifier())
-	if err != nil {
-		return &providerpb.DeleteResourceResponse{}, status.Error(codes.Internal, err.Error())
-	}
-
-	if err := handler.DeleteResource(ctx, id); err != nil {
-		return &providerpb.DeleteResourceResponse{}, status.Error(codes.Internal, err.Error())
-	}
-
-	return &providerpb.DeleteResourceResponse{}, nil
-}
-
 func main() {
-	plugin.Serve(&plugin.ServeConfig{
-		HandshakeConfig: plugin.HandshakeConfig{
-			ProtocolVersion:  1,
-			MagicCookieKey:   "COOKIE",
-			MagicCookieValue: "hi",
+	plugin.Serve(map[string]plugin.ResourceHandler{
+		"bucket": provider.BucketHandler{
+			BucketGetter:  Bucket{},
+			BucketCreator: Bucket{},
+			BucketUpdator: Bucket{},
+			BucketDeleter: Bucket{},
 		},
-		Plugins: map[string]plugin.Plugin{
-			"backend": &Plugin{
-				BackendServer: &Server{
-					ResourceHandlers: map[string]ResourceHandler{
-						"bucket": provider.BucketHandler{
-							BucketGetter:  Bucket{},
-							BucketCreator: Bucket{},
-							BucketUpdator: Bucket{},
-							BucketDeleter: Bucket{},
-						},
-						"bucket_object": provider.BucketObjectHandler{
-							BucketObjectGetter:  BucketObject{},
-							BucketObjectCreator: BucketObject{},
-							BucketObjectUpdator: BucketObject{},
-							BucketObjectDeleter: BucketObject{},
-						},
-					},
-				},
-			},
+		"bucket_object": provider.BucketObjectHandler{
+			BucketObjectGetter:  BucketObject{},
+			BucketObjectCreator: BucketObject{},
+			BucketObjectUpdator: BucketObject{},
+			BucketObjectDeleter: BucketObject{},
 		},
-		GRPCServer: plugin.DefaultGRPCServer,
 	})
-}
-
-type Plugin struct {
-	plugin.Plugin
-
-	BackendServer providerpb.ProviderServer
-}
-
-func (p *Plugin) GRPCServer(_ *plugin.GRPCBroker, s *grpc.Server) error {
-	providerpb.RegisterProviderServer(s, p.BackendServer)
-	return nil
-}
-
-func (p *Plugin) GRPCClient(_ context.Context, _ *plugin.GRPCBroker, conn *grpc.ClientConn) (any, error) {
-	return providerpb.NewProviderClient(conn), nil
 }
--- sdk/provider/plugin/plugin.go
+++ sdk/provider/plugin/plugin.go
@@ -8,15 +8,11 @@ import (
 
 	"github.com/hashicorp/go-plugin"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-type Plugin struct {
-	plugin.Plugin
-
-	server providerpb.ProviderServer
-}
-
-func (p *Plugin) Serve() {
+func Serve(handlers map[string]ResourceHandler) {
 	plugin.Serve(&plugin.ServeConfig{
 		HandshakeConfig: plugin.HandshakeConfig{
 			ProtocolVersion:  1,
@@ -24,40 +20,127 @@ func (p *Plugin) Serve() {
 			MagicCookieValue: "hi",
 		},
 		Plugins: map[string]plugin.Plugin{
-			"provider": p,
+			"provider": plug{
+				server: &server{
+					resourceHandlers: handlers,
+				},
+			},
 		},
 		GRPCServer: plugin.DefaultGRPCServer,
 	})
 }
 
-func (p *Plugin) GRPCServer(_ *plugin.GRPCBroker, s *grpc.Server) error {
+type plug struct {
+	plugin.Plugin
+
+	server providerpb.ProviderServer
+}
+
+func (p *plug) GRPCServer(_ *plugin.GRPCBroker, s *grpc.Server) error {
 	providerpb.RegisterProviderServer(s, p.server)
 	return nil
 }
 
-func (p *Plugin) GRPCClient(_ context.Context, _ *plugin.GRPCBroker, conn *grpc.ClientConn) (any, error) {
+func (p *plug) GRPCClient(_ context.Context, _ *plugin.GRPCBroker, conn *grpc.ClientConn) (any, error) {
 	return providerpb.NewProviderClient(conn), nil
 }
 
 type ResourceHandler interface {
-	GetResource(ctx context.Context, identifier value.Type) error
+	GetResource(context.Context, value.Identifier) (value.ResourceValue, error)
+	CreateResource(context.Context, value.Identifier, value.Value) (value.ResourceValue, error)
+	UpdateResource(context.Context, value.Identifier, value.Value) (value.ResourceValue, error)
+	DeleteResource(context.Context, value.Identifier) error
 }
 
 type server struct {
+	resourceHandlers map[string]ResourceHandler
 }
 
 func (s *server) GetResource(ctx context.Context, req *providerpb.GetResourceRequest) (*providerpb.GetResourceResponse, error) {
-	return nil, nil
+	t := req.GetIdentifier().GetIdentifier().GetType()
+	handler, ok := s.resourceHandlers[t]
+	if !ok {
+		return &providerpb.GetResourceResponse{}, status.Error(codes.NotFound, "resource type not found")
+	}
+
+	id, err := value.ParseIdentifierProto(req.GetIdentifier().GetIdentifier())
+	if err != nil {
+		return &providerpb.GetResourceResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	res, err := handler.GetResource(ctx, id)
+	if err != nil {
+		return &providerpb.GetResourceResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	return &providerpb.GetResourceResponse{Resource: res.ToResourceProto()}, nil
 }
 
 func (s *server) CreateResource(ctx context.Context, req *providerpb.CreateResourceRequest) (*providerpb.CreateResourceResponse, error) {
-	return nil, nil
+	t := req.GetIdentifier().GetIdentifier().GetType()
+	handler, ok := s.resourceHandlers[t]
+	if !ok {
+		return &providerpb.CreateResourceResponse{}, status.Error(codes.NotFound, "resource type not found")
+	}
+
+	id, err := value.ParseIdentifierProto(req.GetIdentifier().GetIdentifier())
+	if err != nil {
+		return &providerpb.CreateResourceResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	config, err := value.ParseProto(req.GetConfig())
+	if err != nil {
+		return &providerpb.CreateResourceResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	res, err := handler.CreateResource(ctx, id, config)
+	if err != nil {
+		return &providerpb.CreateResourceResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	return &providerpb.CreateResourceResponse{Resource: res.ToResourceProto()}, nil
 }
 
 func (s *server) UpdateResource(ctx context.Context, req *providerpb.UpdateResourceRequest) (*providerpb.UpdateResourceResponse, error) {
-	return nil, nil
+	t := req.GetIdentifier().GetIdentifier().GetType()
+	handler, ok := s.resourceHandlers[t]
+	if !ok {
+		return &providerpb.UpdateResourceResponse{}, status.Error(codes.NotFound, "resource type not found")
+	}
+
+	id, err := value.ParseIdentifierProto(req.GetIdentifier().GetIdentifier())
+	if err != nil {
+		return &providerpb.UpdateResourceResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	config, err := value.ParseProto(req.GetConfig())
+	if err != nil {
+		return &providerpb.UpdateResourceResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	res, err := handler.UpdateResource(ctx, id, config)
+	if err != nil {
+		return &providerpb.UpdateResourceResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	return &providerpb.UpdateResourceResponse{Resource: res.ToResourceProto()}, nil
 }
 
 func (s *server) DeleteResource(ctx context.Context, req *providerpb.DeleteResourceRequest) (*providerpb.DeleteResourceResponse, error) {
-	return nil, nil
+	t := req.GetIdentifier().GetIdentifier().GetType()
+	handler, ok := s.resourceHandlers[t]
+	if !ok {
+		return &providerpb.DeleteResourceResponse{}, status.Error(codes.NotFound, "resource type not found")
+	}
+
+	id, err := value.ParseIdentifierProto(req.GetIdentifier().GetIdentifier())
+	if err != nil {
+		return &providerpb.DeleteResourceResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	if err := handler.DeleteResource(ctx, id); err != nil {
+		return &providerpb.DeleteResourceResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	return &providerpb.DeleteResourceResponse{}, nil
 }
